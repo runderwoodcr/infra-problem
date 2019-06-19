8ead0ab32bb646c46de276130fa5b38d45a5440a
Ricardo Underwood <runderwood@pop-os.localdomain>
2019-06-19 10:21:55 -0600

    fixing incorrect name for coomand in config-map

--- iac/modules/aws/outputs.tf
+++ iac/modules/aws/outputs.tf
@@ -45,7 +45,7 @@ users:
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1alpha1
-      command: iam-authenticator-aws
+      command: aws-iam-authenticator
       args:
         - "token"
         - "-i"
